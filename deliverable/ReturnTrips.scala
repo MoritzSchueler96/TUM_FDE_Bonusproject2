ad12ceb806e0a960a92a877510149b301f093532
MoritzSchueler96 <47536226+MoritzSchueler96@users.noreply.github.com>
2021-01-18 21:10:54 +0100

    upload current state

--- deliverable/ReturnTrips.scala
+++ deliverable/ReturnTrips.scala
@@ -9,12 +9,143 @@ import org.apache.spark.sql.Dataset
 import org.apache.spark.sql.Row
 
 object ReturnTrips {
-  def compute(trips : Dataset[Row], dist : Double, spark : SparkSession) : Dataset[Row] = {
+  def compute(
+      trips: Dataset[Row],
+      dist: Double,
+      spark: SparkSession
+  ): Dataset[Row] = {
 
     import spark.implicits._
+    val diff_time = 28800
+    val diff_dist = dist * 360 / 6371000
 
-    ToDo: Add your implementation here
+    val trips_filtered = trips
+      .select(
+        "pickup_longitude",
+        "pickup_latitude",
+        "dropoff_longitude",
+        "dropoff_latitude",
+        "tpep_pickup_datetime",
+        "tpep_dropoff_datetime"
+      )
+      .withColumn(
+        "tpep_pickup_datetime",
+        unix_timestamp($"tpep_pickup_datetime")
+      )
+      .withColumn(
+        "tpep_dropoff_datetime",
+        unix_timestamp($"tpep_dropoff_datetime")
+      )
 
-    trips
+    val trips_final = trips_filtered
+      .as(alias = "a")
+      .withColumn(
+        "dropoff_time_bucket",
+        floor($"a.tpep_dropoff_datetime" / diff_time)
+      )
+      .withColumn(
+        "pickup_lat_bucket",
+        explode(
+          array(
+            floor($"a.pickup_latitude" / diff_dist) - 1,
+            floor($"a.pickup_latitude" / diff_dist),
+            floor($"a.pickup_latitude" / diff_dist) + 1
+          )
+        )
+      )
+      .withColumn(
+        "pickup_lon_bucket",
+        explode(
+          array(
+            floor($"a.pickup_longitude" / diff_dist) - 1,
+            floor($"a.pickup_longitude" / diff_dist),
+            floor($"a.pickup_longitude" / diff_dist) + 1
+          )
+        )
+      )
+      .join(
+        trips_filtered
+          .as(alias = "b")
+          .withColumn(
+            "pickup_time_bucket",
+            explode(
+              array(
+                floor($"b.tpep_pickup_datetime" / diff_time) - 1,
+                floor($"b.tpep_pickup_datetime" / diff_time)
+              )
+            )
+          )
+          .withColumn(
+            "dropoff_lat_bucket",
+            floor($"b.dropoff_latitude" / diff_dist)
+          )
+          .withColumn(
+            "dropoff_lon_bucket",
+            floor($"b.dropoff_longitude" / diff_dist)
+          ),
+        // Bucket equi join
+        $"dropoff_time_bucket" === $"pickup_time_bucket" &&
+          $"pickup_lat_bucket" === $"dropoff_lat_bucket" &&
+          $"pickup_lon_bucket" === $"dropoff_lon_bucket",
+        "inner"
+      )
+      .drop(
+        "dropoff_time_bucket",
+        "pickup_lat_bucket",
+        "pickup_lon_bucket",
+        "pickup_time_bucket",
+        "pickup_lat_bucket",
+        "pickup_lon_bucket"
+      )
+      .filter(
+        // Condition 1
+        $"a.tpep_dropoff_datetime" > $"b.tpep_pickup_datetime" - diff_time &&
+          $"b.tpep_pickup_datetime" > $"a.tpep_dropoff_datetime" &&
+          // Condition 2
+          asin(
+            sqrt(
+              pow(
+                sin(
+                  toRadians($"b.pickup_latitude" - $"a.dropoff_latitude") / 2
+                ),
+                2
+              ) +
+                cos(toRadians($"b.pickup_latitude")) * cos(
+                  toRadians($"a.dropoff_latitude")
+                ) *
+                pow(
+                  sin(
+                    toRadians(
+                      $"b.pickup_longitude" - $"a.dropoff_longitude"
+                    ) / 2
+                  ),
+                  2
+                )
+            )
+          ) * 2 * 6371000 < lit(dist) &&
+          // Condition 3
+          asin(
+            sqrt(
+              pow(
+                sin(
+                  toRadians($"b.dropoff_latitude" - $"a.pickup_latitude") / 2
+                ),
+                2
+              ) +
+                cos(toRadians($"b.dropoff_latitude")) * cos(
+                  toRadians($"a.pickup_latitude")
+                ) *
+                pow(
+                  sin(
+                    toRadians(
+                      $"b.dropoff_longitude" - $"a.pickup_longitude"
+                    ) / 2
+                  ),
+                  2
+                )
+            )
+          ) * 2 * 6371000 < lit(dist)
+      )
+    trips_final
   }
 }
